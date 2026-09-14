ea1f1db832889ac3e55be0d04cfb7162d02f8684
Bradshaw Asher <71607148+BradshawAsher@users.noreply.github.com>
2026-09-14 12:25:26 -0700

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.py
@@ -0,0 +1,42 @@
+from collections import deque
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        
+        visited = [[False] * cols for _ in range(rows)]
+        max_island_size = 0
+
+        #do it the bfs way
+        def bfs(start_r: int, start_c: int) -> int:
+            queue = deque([(start_r, start_c)])
+            visited[r][c] = True
+
+            cur_island_size = 0
+            #explore neighbors
+            neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            while queue:
+                cur_island_size += 1
+
+                cur_r, cur_c = queue.popleft()
+                for dr, dc in neighbors:
+                    nr, nc = cur_r + dr, cur_c + dc
+
+                    #check bounds
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1 and not visited[nr][nc]:
+                        visited[nr][nc] = True
+                        queue.append((nr, nc))
+
+            return cur_island_size
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and not visited[r][c]:
+                    
+                    max_island_size = max(bfs(r, c), max_island_size)
+        
+        return max_island_size
