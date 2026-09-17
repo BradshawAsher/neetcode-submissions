26c4243fdda0a1717f076af2860297d3b2a6c415
Bradshaw Asher <71607148+BradshawAsher@users.noreply.github.com>
2026-09-17 12:06:22 -0700

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #1. Find pivot (index of min value)
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        pivot = left
+
+        #2. Decide which sorted segment contains target
+        if nums[pivot] <= target <= nums[-1]:
+            left, right = pivot, len(nums) - 1
+        else:
+            left, right = 0, pivot - 1
+
+        #3. Standard vanilla binary search
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
